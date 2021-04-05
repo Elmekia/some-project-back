99586fd58fa5666212e33412389dc25a5ab3b56a
Matias de la Iglesia <matias.delaiglesia@technolobiz.com>
2021-04-05 19:31:04 -0300

    commit initial

---
+++ src/test/java/com/emk/cco/CcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emk.cco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
